0ed69236f2548f9356a9913c20d90f8a10a7488d
andres950615 <bafdurango1995@hotmail.com>
2021-10-24 06:51:29 -0500

    Cors

--- index.php
+++ index.php
@@ -4,6 +4,16 @@
 
 error_reporting(E_ALL);
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    //die();
+}
+
+
 header('Content-Type: application/json; charset=utf-8');
 
 if(isset($_GET['controller']) && isset($_GET['method'])) {
